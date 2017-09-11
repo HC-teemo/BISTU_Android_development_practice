10d2a6fb177da2b835b1c9f3ffee3673a953becd
huchuan <2585814664@qq.com>
2017-09-11 14:08:30 +0800

    update

--- ImplicitIntentTest/app/src/main/java/com/example/huchuan/implicitintenttest/MainActivity.java
+++ ImplicitIntentTest/app/src/main/java/com/example/huchuan/implicitintenttest/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.huchuan.implicitintenttest;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button=(Button)this.findViewById(R.id.button);
+        Button button2=(Button)this.findViewById(R.id.button2);
+        final EditText editText=(EditText)this.findViewById(R.id.editText);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -20,6 +32,23 @@ public class MainActivity extends AppCompatActivity {
                 intent.setAction("startAnotherActivity");
                 intent.addCategory("android.intent.category.DEFAULT");
                 startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this, "缺少打电话权限", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String telnum=editText.getText().toString();
+                String encodeTelnum=null;
+                try{
+                    encodeTelnum= URLEncoder.encode(telnum,"UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+encodeTelnum)));
             }
         });
     }
